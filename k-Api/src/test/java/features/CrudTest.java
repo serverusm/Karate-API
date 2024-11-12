d12a421b834138e574f8227b940986b01aebc837
ServerusM <sergiomamani2014@gmail.com>
2024-11-12 11:22:44 -0400

    Changes in CrudTest and TestsRunner

--- k-Api/src/test/java/features/CrudTest.java
+++ k-Api/src/test/java/features/CrudTest.java
@@ -1,15 +1,15 @@
-package examples;
+package features;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class ExamplesTest {
+class CrudTest {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:examples")
+        Results results = Runner.path("classpath:features")
                 //.outputCucumberJson(true)
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
